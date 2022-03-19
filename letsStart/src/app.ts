594c1fedef5f0a396544c2006e46dc1456216b80
nno3onn <nno3onn@naver.com>
2022-03-19 12:51:20 +0900

    test

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,1 +1,15 @@
-console.log("hello world!!");
+import * as express from "express";
+
+const app: express.Express = express();
+
+const port: number = 3000;
+
+// router
+app.get("/test", (req: express.Request, res: express.Response) => {
+  console.log(req);
+  res.send({ name: "heo da eun", age: 99, friends: ["1", "ss", "hi", "bb"] });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

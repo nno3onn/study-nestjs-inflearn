84c19d444b320153d99e6caa80fbc4163098eb37
nno3onn <nno3onn@naver.com>
2022-03-19 14:38:11 +0900

    study middleware

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -5,11 +5,43 @@ const app: express.Express = express();
 
 const port: number = 3000;
 
+// middleware (add next)
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(req.rawHeaders[1]);
+    console.log("this is logging middleware");
+    next();
+  }
+);
+
+app.get(
+  "/cats/som",
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(req.rawHeaders[1]);
+    console.log("this is som middleware");
+    next();
+  }
+);
+
 // router
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
   res.send({ cats: [...Cat] });
 });
+
+app.get("/cats/blue", (req: express.Request, res: express.Response) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get("/cats/som", (req: express.Request, res: express.Response) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log("this is error middleware");
+    res.send({ error: "404 not found error" });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
